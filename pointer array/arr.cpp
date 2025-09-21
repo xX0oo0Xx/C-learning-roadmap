089d27dad07eef519f1094af477feabfdf3bf960
xX0oo0Xx <siyuliang@uvic.ca>
2025-09-21 15:03:29 -0700

    pointer

---
+++ pointer array/arr.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+string getHex(int x){
+    char buf[10];
+    sprintf(buf, "%X", (x & 0xFFFF));
+    return string(buf);
+}
+int main(){
+    int arr[3][4] = {
+        {1,2,3,4},
+        {5,6,7,8},
+        {9,10,11,12}
+    };
+
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 4; j++){
+            if(j){
+                cout << ",";
+            }
+            int *p = &arr[i][j];
+            cout << getHex((int)p);
+        }
+        cout << endl;
+
+    int* q[3]={arr[0],arr[1],arr[2]};
+    int (*p)[4];
+    p = &arr[0];
+
+    }
+}
